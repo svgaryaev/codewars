fca4960585716df1584739ebeeae437274a9fd41
svgaryaev <svgaryaev@gmail.com>
2019-12-25 20:29:37 +0300

    4kyu large factorials update

--- 4kyu/large-factorials/solution.js
+++ 4kyu/large-factorials/solution.js
@@ -1,4 +1,18 @@
 function factorial(n) {
+    if (n < 0) return null;
+    let res = [1];
+    for (let i = 1; i <= n; i++) {
+        for (let j = 0, carry = 0; j < res.length || carry > 0; j++) {
+            carry += (res[j] || 0) * i;
+            res[j] = carry % 10;
+            carry = carry / 10 | 0;
+        }
+    }
+    return res.reverse().join('');
+}
+
+/*
+function factorial(n) {
     function bigMul(bigNum, k) {
         let carry = 0;
         for (let i = bigNum.length - 1; i >= 0; i--) {
@@ -19,3 +33,4 @@ function factorial(n) {
     }
     return acc.join('');
 }
+*/
